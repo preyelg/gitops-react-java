b811c5e52e1036db6ac14398a02658f1df52b705
preyelg <geeftedpl@gmail.com>
2025-06-03 15:48:54 -0500

    updated

--- backend/src/main/java/com/cyat/backend/Main.java
+++ backend/src/main/java/com/cyat/backend/Main.java
@@ -1,7 +1,20 @@
 package com.cyat.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Backend service for GitOps project running...");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend service for GitOps project running...";
     }
 }
